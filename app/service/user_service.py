427bce30f78b21965d862d159da79356622d9bf2
Thịnh <nam.tb@wfc.vn>
2022-12-06 10:20:28 +0700

    Revert "thinh commit"
    This reverts commit 4c57fcd84ae5efcf5d99b576777fdb55d57bb92c.

--- app/service/user_service.py
+++ app/service/user_service.py
@@ -1,20 +1,11 @@
 from app.model.db import userCollection
 from app.model.model import Users
-from app.apis.util.exception import NotFoundDataException
-from ..util.helpers import _throw
-from bson.objectid import ObjectId
+
 
 class UserService:
     def __init__(self) -> None:
         pass
 
-    @staticmethod
-    def get_user_by_id(id):
-        try:
-            user = userCollection.find_one({'_id': ObjectId(id)})
-            return user
-        except NotFoundDataException as e:
-            _throw(e)
     # def authenticate(username: str, password: str):
     #     if(not username or not password):
     #         raise Exception("missing field ")
